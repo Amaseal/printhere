cb4cd8ea272d255daac9fdc57140678e3f9cc98d
Arturs <freelance.jershov@gmail.com>
2022-09-08 14:13:44 +0300

    header/desktop navigation

---
+++ src/lib/components/header/Header.svelte
@@ -0,0 +1,61 @@
+<script>
+  import DesktopNav from "$lib/components/header/desktopnav/DesktopNav.svelte";
+  import CartOutline from "svelte-material-icons/CartOutline.svelte";
+  import MobileNav from "$lib/components/header/mobilenav/MobileNav.svelte";
+
+  let width;
+  let size = "1.5em";
+</script>
+
+<svelte:window bind:innerWidth={width} />
+
+<header>
+  <div class="container flex align">
+    <a href="/">
+      <img src="./logo.svg" alt="PrintHere logo" />
+    </a>
+
+    {#if width > 900}
+      <DesktopNav />
+    {:else}
+      <MobileNav />
+    {/if}
+    <button class="cart">
+      <span>0</span>
+      <CartOutline {size} />
+    </button>
+  </div>
+</header>
+
+<style>
+  img {
+    width: 180px;
+  }
+  .cart {
+    display: flex;
+    padding: 10px;
+    border-radius: 50%;
+    transition: background-color 0.2s ease;
+    position: relative;
+    cursor: pointer;
+    background-color: transparent;
+    margin-left: 10px;
+  }
+  .cart:hover {
+    background-color: var(--secondary-color);
+  }
+  span {
+    position: absolute;
+    font-size: 14px;
+    font-weight: 600;
+    text-align: center;
+    padding-top: 2px;
+    width: 20px;
+    height: 20px;
+    border-radius: 10px;
+    top: 0;
+    right: 0;
+    background-color: var(--primary-color);
+    cursor: pointer;
+  }
+</style>
---
+++ src/lib/components/header/desktopnav/DesktopNav.svelte
@@ -0,0 +1,22 @@
+<nav class="flex align">
+  <a href="/products">Products</a>
+  <a href="/about">About</a>
+  <button>Order</button>
+</nav>
+
+<style>
+  a {
+    padding: 5px 10px;
+  }
+  a:first-of-type {
+    margin-left: auto;
+  }
+
+  nav {
+    height: 100%;
+    width: 100%;
+  }
+  button {
+    margin-left: auto;
+  }
+</style>
